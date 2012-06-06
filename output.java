d39a41204dbc028ce495db647854152f13c22b00
Dan Fickle <newtab@gmail.com>
2012-06-06 18:25:20 +1000

    Added test.cpp test file and the output(output.java). Note the output has been formatted with Eclipse.

---
+++ output.java
@@ -0,0 +1,175 @@
+package yourpackage;
+
+class Globals {
+	class class_outside_namespace {
+		byte a;
+		char b;
+		short c;
+		int d;
+		int e;
+		long f;
+		int g;
+		short h;
+		long i;
+	}
+}
+
+class mypair<T> {
+	T[] values;
+
+	public mypair(T first, T second) {
+		values[0].op_assign(first);
+		values[1].op_assign(second);
+	}
+}
+
+class HashMap<T, V> {
+	T one = new T();
+	V two = new V();
+}
+
+class CT<V, T> {
+	HashMap<V, Integer> three = new HashMap<V, Integer>();
+	T one = new T();
+	V two = new V();
+}
+
+enum AnonEnum0 {
+	value1(0), value2(1);
+	final int val;
+
+	AnonEnum0(final int enumVal) {
+		val = enumVal;
+	}
+
+	static AnonEnum0 fromValue(final int enumVal) {
+		switch (enumVal) {
+		case 0:
+			return value1;
+		case 1:
+			return value2;
+		default:
+			throw new ClassCastException();
+		}
+	}
+}
+
+enum AnonEnum1 {
+	value3(0), value4(1);
+	final int val;
+
+	AnonEnum1(final int enumVal) {
+		val = enumVal;
+	}
+
+	static AnonEnum1 fromValue(final int enumVal) {
+		switch (enumVal) {
+		case 0:
+			return value3;
+		case 1:
+			return value4;
+		default:
+			throw new ClassCastException();
+		}
+	}
+}
+
+class foo {
+	public foo() {
+	}
+
+	public foo(int i) {
+		i = AnonEnum0.value1.val;
+		i = AnonEnum1.value4.val;
+	}
+
+	public foo(int i, short t) {
+	}
+
+	public int func_with_defaults(int one) {
+		return func_with_defaults(one, 5);
+	}
+
+	public int func_with_defaults_and_definition(int one, int two) {
+		two = 4;
+		return two;
+	}
+
+	public int func_with_defaults_and_definition(int one) {
+		return func_with_defaults_and_definition(one, 3);
+	}
+}
+
+class test extends foo {
+	enum test_enum {
+		val1(0), val2(1020), val3((1020) + 1), val4(1045);
+		final int val;
+
+		test_enum(final int enumVal) {
+			val = enumVal;
+		}
+
+		static test_enum fromValue(final int enumVal) {
+			switch (enumVal) {
+			case 0:
+				return val1;
+			case 1020:
+				return val2;
+			case (1020) + 1:
+				return val3;
+			case 1045:
+				return val4;
+			default:
+				throw new ClassCastException();
+			}
+		}
+	}
+
+	class subby {
+		int k;
+	}
+
+	foo test_foo_with_init = new foo();
+
+	public int get__test_with_bit_field() {
+		return __bitfields & 1;
+	}
+
+	public void set__test_with_bit_field(int val) {
+		__bitfields &= ~1;
+		__bitfields |= (val << 0) & 1;
+	}
+
+	public int func2() {
+		test_with_bit_field = 8;
+		return test_with_bit_field;
+	}
+
+	public int func3(foo a,foo b,Ptr<foo> c,foo d,foo e,RefInteger f){foo j;while ((j=b) != null){}foo l;if ((l=b) != null){}for (foo a5=b;(a5) != null;a5=b){}while ((b) != null){}if ((b) != null){}for (int i=0;i < 10;i++){for (i=1;i < 5;i++){break;}continue;}int i=test_enum.val1.val + test_enum.val2.val;PtrByte ptr1;Ptr<int> ptr2;PtrShort ptr3;PtrInteger ptr4;PtrBoolean ptr5;PtrInteger ptr7;PtrInteger ptr6;p="";p=ptr6.val;return 1072;}	public foo func4(int a, int b, short c) {
+		foo[] sd = new foo[15];
+		for (int gen___i0 = 0; gen___i0 < sd.length; gen__i0++) {
+			sd[gen___i0] = new foo();
+		}
+		foo foo1;
+		Ptr<foo> foo2;
+		foo foo3 = new foo(sd[1]);
+		foo foo4 = new foo(1, 2);
+		foo foo5 = new foo(foo4);
+		foo foo6 = this;
+		foo foo7 = new foo(this);
+		int d = (1 + 2) * 4;
+		boolean mybool1 = (foo1) != null && (foo6) != null && true;
+		boolean mybool2 = (foo1) != null || (foo6) != null || false;
+		func2();
+		d += 10;
+		switch (d) {
+		}
+		foo6.func_with_defaults_and_definition(100);
+		(foo6).func_with_defaults_and_definition(100, 2);
+		return new foo(sd[2]);
+	}
+
+	public foo func4(int a, int b) {
+		return func4(a, b, 55);
+	}
+}
---
+++ test.cpp
@@ -0,0 +1,237 @@
+// Test function outside namespace...
+int func_outside_namespace();
+
+// Test class outside namespace...
+class class_outside_namespace
+{
+	// Test basic types...
+	char a;
+	wchar_t b;
+	short c;
+	int d;
+	long e;
+	long long f;
+	unsigned g;
+	unsigned short h;
+	unsigned long long i;
+};
+
+// Test variables outside namespace...
+class_outside_namespace cls1;
+int variable;
+
+// Test a namespace...
+namespace shit 
+{
+
+// Test basic template class...
+template <class T>
+class mypair {
+	// Test array of template parameter types...
+	T values [2]; 
+	// Test access modifier...
+	public:
+	// Test constructor using template parameter types...
+	mypair (T first, T second)
+	{
+		// Test array access...
+		values[0] = first;
+		values[1] = second;
+	}
+};
+
+// Test template class with two parameters...
+template <class T, class V>
+class HashMap
+{
+	T one;
+	V two;
+};
+
+template<class V, class T> class CT
+{
+	// Test using template types in as template parameters...
+	HashMap<V, int> three;
+	T one;
+	V two;
+
+};
+
+// Test anonymous enums...
+enum { value1, value2 };
+enum { value3, value4 };
+
+class foo
+{
+	foo() { }
+	foo(int i) {
+		// Test use of anonymous enums values
+		// and assigning enum values to int...
+		i = value1;
+		i = value4;
+	}
+	foo(int i, short t) {}
+	
+	// Test a function declaration with default values...
+	int func_with_defaults(int one, int two = 5);
+
+	// Test a function definition with default values...
+	int func_with_defaults_and_definition(int one, int two = 3)
+	{
+		two = 4;
+		// Test a return statement...
+		return two;
+	}
+};
+
+// Test a class with a parent class...
+class test : public foo {
+
+	// Test an enclosed enumeration...
+	enum test_enum
+	{
+		val1,
+		// Test giving enumerators values...
+		val2 = 1020,
+		val3,
+		val4 = 1045
+	};
+
+	// Test an enclosed class...
+	class subby
+	{
+		int k;
+	};
+
+	// Test value in class with implicit constructor...
+	foo test_foo_with_init;
+
+	// Test value in class with initializer value...
+	foo test_foo_with_int_constructor(55);
+
+	// Test bit fields...
+	int test_with_bit_field : 5;
+
+	int func2()
+	{
+		// Test setting of bit field...
+		test_with_bit_field = 8;
+		// Test getting of bit field...
+		return test_with_bit_field;
+	}
+
+	// Test function for pointers and references...
+	int func3(foo a, foo * b, foo ** c, foo& d, const foo& e, int& f)
+	{
+		// Test declaration in while...
+		while (foo* j = b) { }
+
+		// Test declaration in if...
+		if (foo * l = b) { }
+
+		// Test declaration in for...
+		for (foo * a5 = b;a5;a5 = b) { }
+
+		// Test while...
+		while (b) { }
+
+		// Test if...
+		if (b) { }
+
+		// Test standard looking for...
+		for (int i = 0; i < 10; i++) 
+		{
+			// Test nested for...
+			for (i = 1; i < 5; i++)
+			{
+				// Test break statement...
+				break;
+			}
+			// Test continue statement...
+			continue;
+		}
+
+		// Test qualified enum access...
+		int i = test_enum::val1 + test_enum::val2;
+
+		// Test various pointer to basic types...
+		char * ptr1;
+		int ** ptr2;
+		short * ptr3;
+		unsigned * ptr4;
+		bool * ptr5;
+		// Test multiple declarators on with one declaration...
+		int * ptr7, * ptr6;
+
+		// Test address of operator...
+		p = &i;
+
+		// Test basic pointer assignment...
+		p = ptr6;
+
+		// Test number literals...
+		return 1072;
+	}
+
+	foo func4(int a, int b, short c = 55)
+	{
+		// Test implicity constructors with array...
+		foo sd[15];
+
+		// Test pointers to composite type...
+		foo * foo1;
+		foo ** foo2;
+
+		// Test references to composite type...
+		foo& foo3 = sd[1];
+
+		// Test constructor call...
+		foo foo4(1, 2);
+
+		// Test implicity copy constructor call...
+		foo foo5 = foo4;
+
+		// Test pointer assignment from this...
+		foo * foo6 = this;
+
+		// Test copy from this...
+		foo foo7 = *this;
+
+		// Test bracketed expression...
+		int d = (1 + 2) * 4;
+
+		// Test && with non boolean expression...
+		bool mybool1 = foo1 && foo6 && true;
+
+		// Test || with non boolean expression...
+		bool mybool2 = foo1 || foo6 || false;
+
+		// Test function call...
+		func2();
+
+		// Test +=...
+		d += 10;
+
+		// Test switch...
+		switch (d)
+		{
+			// Test fall through...
+			case 1:
+			case 2:
+				break;
+			default:
+				return foo7;
+		}
+
+		// Test -> operator...
+		foo6->func_with_defaults_and_definition(100);
+
+		// Test dereference and call...
+		(*foo6).func_with_defaults_and_definition(100, 2);
+
+		// Test copy constructor on return...
+		return sd[2];
+	}
+};
+};
+
