dcddde7f055f7ac50cb1627b50a9ea95e937142c
Dan Fickle <newtab@gmail.com>
2012-06-08 20:00:21 +1000

    Added latest version of manually formatted output.java.

--- output.java
+++ output.java
@@ -1,7 +1,7 @@
 package yourpackage;
 
 class Globals {
-	class class_outside_namespace {
+	class class_outside_namespace implements HasDestructor {
 		byte a;
 		char b;
 		short c;
@@ -18,7 +18,7 @@ class Globals {
 	AnonClass3 anon_class3 = new AnonClass3();
 }
 
-class CppString {
+class CppString implements HasDestructor {
 	int m_count;
 
 	public CppString(int i) {
@@ -26,7 +26,7 @@ class CppString {
 	}
 }
 
-class mypair<T> {
+class mypair<T> implements HasDestructor {
 	T[] values;
 
 	public mypair(T first, T second) {
@@ -35,12 +35,12 @@ class mypair<T> {
 	}
 }
 
-class HashMap<T, V> {
+class HashMap<T, V> implements HasDestructor {
 	T one = new T();
 	V two = new V();
 }
 
-class CT<V, T> {
+class CT<V, T> implements HasDestructor {
 	HashMap<V, Integer> three = new HashMap<V, Integer>();
 	T one = new T();
 	V two = new V();
@@ -86,8 +86,11 @@ enum AnonEnum1 {
 	}
 }
 
-class foo {
+class foo implements HasDestructor {
 	public foo() {
+	}
+
+	public void destruct() {
 	}
 
 	public foo(int i) {
@@ -98,8 +101,12 @@ class foo {
 	public foo(int i, short t) {
 	}
 
-	public int func_with_defaults(int one) {
-		return func_with_defaults(one, 5);
+	public int func_with_defaults1(int one) {
+		return func_with_defaults1(one, 5);
+	}
+
+	public void func_with_defaults2() {
+		func_with_defaults2(7);
 	}
 
 	public int func_with_defaults_and_definition(int one, int two) {
@@ -112,7 +119,7 @@ class foo {
 	}
 }
 
-class test extends foo {
+class test extends foo implements HasDestructor {
 	enum test_enum {
 		val1(0), val2(1020), val3((1020) + 1), val4(1045);
 		final int val;
@@ -137,7 +144,7 @@ class test extends foo {
 		}
 	}
 
-	class AnonClass0 {
+	class AnonClass0 implements HasDestructor {
 		int a;
 		int b;
 	}
@@ -145,7 +152,7 @@ class test extends foo {
 	AnonClass0 anon_class1 = new AnonClass0();
 	AnonClass0 anon_class2 = new AnonClass0();
 
-	class AnonClass1 {
+	class AnonClass1 implements HasDestructor {
 		int c;
 		int d;
 	}
@@ -153,7 +160,7 @@ class test extends foo {
 	AnonClass1 anon_class3 = new AnonClass1();
 	AnonClass1 anon_class4 = new AnonClass1();
 
-	class subby {
+	class subby implements HasDestructor {
 		int k;
 	}
 
@@ -173,15 +180,13 @@ class test extends foo {
 		return test_with_bit_field;
 	}
 
-	public int func3(foo a,foo b,Ptr<foo> c,foo d,foo e,RefInteger f)
-	{
+	public int func3(foo a,foo b,Ptr<foo> c,foo d,foo e,RefInteger f){
 		foo j;
 		while ((j=b) != null){}
-		foo l;
-		if ((l=b) != null){}
+		foo l;if ((l=b) != null){}
 		for (foo a5=b;(a5) != null;a5=b){}
-		while ((b) != null){}
-		if ((b) != null){}for (int i=0;i < 10;i++){
+		while ((b) != null){}if ((b) != null){}
+		for (int i=0;i < 10;i++){
 			for (i=1;i < 5;i++){
 				break;
 			}
@@ -200,7 +205,7 @@ class test extends foo {
 		ptr8=ptr7;
 		return 1072;
 	}
-
+	
 	public foo func4(int a, int b, short c) {
 		foo[] sd = new foo[15];
 		for (int gen___i0 = 0; gen___i0 < sd.length; gen__i0++) {
@@ -219,6 +224,17 @@ class test extends foo {
 		func2();
 		d += 10;
 		switch (d) {
+		case 1:
+		case 2:
+			break;
+		default:
+			return new foo(foo7);
+		}
+		int decl;
+		switch (decl = 1) {
+		case 1:
+			decl += 5;
+			break;
 		}
 		foo6.func_with_defaults_and_definition(100);
 		(foo6).func_with_defaults_and_definition(100, 2);
@@ -229,15 +245,43 @@ class test extends foo {
 		return func4(a, b, 55);
 	}
 
+	public void func5() {
+		int i = 0;
+		if (true)
+			i += 5;
+		while (false)
+			i -= 10;
+		for (i = 0; i < 20; i++)
+			i--;
+		switch (i) {
+		case 1:
+			return;
+		case 2:
+			i++;
+		}
+		if (true)
+			for (;;)
+				while (true)
+					;
+		for (int k = 0; (k < 10) != 0; k++)
+			k *= 10;
+		foo ptr1 = new foo();
+		foo ptr2 = new foo(100);
+		foo ptr3 = new foo();
+		DestructHelper.destructItems(ptr1);
+		DestructHelper.destructItems(ptr2);
+		DestructHelper.destructArray(ptr3);
+	}
+
 	/**
 	 * @union
 	 */
-	class test_union {
+	class test_union implements HasDestructor {
 		int a;
 		float b;
 	}
 
-	class test_struct {
+	class test_struct implements HasDestructor {
 		int a;
 		int b;
 	}
@@ -246,13 +290,13 @@ class test extends foo {
 /**
  * @union
  */
-class test_union2 {
+class test_union2 implements HasDestructor {
 	short a;
 	short b;
 }
 
-class AnonClass2 {
-	class AnonClass3 {
+class AnonClass2 implements HasDestructor {
+	class AnonClass3 implements HasDestructor {
 		int a;
 	}
 
