730a2cf623debfa2f52d5fa3d1c6ce0f3b317093
Dan Fickle <newtab@gmail.com>
2014-01-18 23:12:25 +1100

    Added some test for function call overloading. Currently these tests are failing.

--- tests/test-operator-overloading.cpp
+++ tests/test-operator-overloading.cpp
@@ -1,26 +1,36 @@
-// Test global operator overloading...
 class OpTest
 {
 public:
 	int j;
 	OpTest(int i) : j(i) { }
+
+	// Binary method
  	OpTest operator -(const OpTest& b) const
 	{
 		return OpTest(j - b.j);
 	}
 
+	// Unary method
 	OpTest operator -()
 	{
 		return OpTest(-j);
 	}
 
+	// Function call method
+	void operator()
+	{
+		j++;
+	}
+
 };
 
+// Binary function
 OpTest operator +(const OpTest& a, const OpTest& b)
 {
 	return OpTest(a.j + b.j);
 }
 
+// Unary function
 OpTest operator +(const OpTest& a)
 {
 	return OpTest(-a.j);
@@ -35,5 +45,6 @@ void OpTestTest()
 	OpTest d = b - a;
 	OpTest e = +a;
 	OpTest f = -a;
+	f();
 }
 
